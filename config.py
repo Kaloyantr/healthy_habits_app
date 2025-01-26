3f640aabbe87d7b96cefc6531b9d069e4788e22b
Kaloyan Trifonov <kaloyantr@abv.bg>
2025-01-27 00:49:32 +0200

    Make blueprint

--- config.py
+++ config.py
@@ -3,7 +3,5 @@ import os
 class Config:
     """Основна конфигурация за приложението"""
     SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "sqlite:///db.sqlite3"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///src/database/db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,50 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for
 
 # Създаване на Blueprint за организиране на маршрутите
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@main.route("/api/health", methods=["GET"])
-def health_status():
-    return jsonify({"status": "OK", "message": "Healthy Habits App is running!"})
+@main.route("/register")
+def register():
+    return render_template('register.html')
+
+@main.route("/register_action", methods=['POST'])
+def register_action():
+    if request.method == 'POST':
+        # Вземаме данните от формата
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # TODO: Добави логика за запис в базата данни тук
+        print(f"New user: {username}, {email}")
+        print("Registration successful!")
+
+    return render_template('login.html')
+
+@main.route("/login_action", methods=['POST'])
+def login_action():
+    if request.method != 'POST':
+        return redirect(url_for('main.login', message='Invalid method'))
+
+    if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+        username = request.form['username']
+        return redirect(url_for('main.user_page', name=username, token='123456'))
+    else:
+        return redirect(url_for('main.login', message='Invalid username or password'))
+
+@main.route("/login")
+def login():
+    message = request.args.get('message', None)
+    return render_template('login.html', title='Login', message=message)
+
+@main.route("/user_page")
+def user_page():
+    return render_template('in_app.html')
+
+@main.route('/logout')
+def logout():
+    return redirect(url_for('main.login'))
